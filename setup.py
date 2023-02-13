4cbb38042400ff9fcdc5c8b234df5eb5555591a5
Andrii Svitelskyi <andriy.svitelskyi@gmail.com>
2023-02-13 13:24:18 +0200

    info

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name = "CleandFolder",
+    version = "0.0.1",
+    author="Andrii Svitelskyi",
+    author_email="andriy.svitelskyi@gmail.com",
+    description = "This app is for organizing your files/folders.",
+    readme = "README.md",
+    requires-python = ">=3.10",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Windows",
+    ]
+
+    "Homepage" = "https://github.com/andriyseeker22856/CleanFolder_App"
+)
